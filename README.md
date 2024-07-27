e563e449f9d47f70a6eee913191506b9716a9680
yashikota <52403688+yashikota@users.noreply.github.com>
2024-07-27 17:16:57 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# kindle-pdf
+
+## Prerequisites
+
+Make sure you [enabled USB debugging](https://developer.android.com/studio/debug/dev-options#enable) on your device.  
+
+## Run
+
+```sh
+./script.ps1
+```
+
+## Convert screenshot to pdf
+
+Use [ImageMagick](https://imagemagick.org).  
+
+```sh
+magick convert img/*.png out.pdf
+```
---
+++ script.ps1
@@ -0,0 +1,26 @@
+$page_number = 1
+
+# Create the img directory if it doesn't exist
+if (-not (Test-Path "img")) {
+    New-Item -ItemType Directory -Path "img"
+}
+
+# Get Screen Resolution
+$screen_resolution = adb shell wm size
+$screen_resolution = $screen_resolution -replace "Physical size: ", ""
+$screen_resolution = $screen_resolution -replace "x", " "
+$screen_resolution = $screen_resolution -split " "
+$screen_width = [int]$screen_resolution[0]
+$screen_height = [int]$screen_resolution[1]
+
+# Tap coordinates
+$tap_x = $screen_width - 100
+$tap_y = $screen_height / 2
+
+while ($true) {
+    $formatted_page_number = $page_number.ToString("D4")
+    adb exec-out screencap -p > "img/$formatted_page_number.png"
+    adb shell input touchscreen tap $tap_x $tap_y
+    Start-Sleep -Seconds 0.25
+    $page_number++
+}
